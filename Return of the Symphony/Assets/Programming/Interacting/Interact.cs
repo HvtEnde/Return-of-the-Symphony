0db711f3113b3bc71cda81b2f14aae5582f5d1e0
HvtEnde <97097945@st.deltion.nl>
2024-11-27 16:00:55 +0100

    Interact

--- Return of the Symphony/Assets/Programming/Interacting/Interact.cs
+++ Return of the Symphony/Assets/Programming/Interacting/Interact.cs
@@ -5,25 +5,52 @@ using UnityEngine.InputSystem;
 
 public class Interact : MonoBehaviour
 {
-    private PlayerInput _playerInput;
+    #region Interact Variables
+    [SerializeField] private LayerMask interactableLayer;
+    private PlayerInput playerInput;
+    private Transform _transform;
+    #endregion
 
+
+    /// <summary>
+    /// Assigns the transform and playerinput
+    /// </summary>
     private void Awake()
     {
-        _playerInput = GetComponent<PlayerInput>();
+        _transform = transform;
+
+        playerInput = GetComponent<PlayerInput>();
     }
+
+    /// <summary>
+    /// When E is pressed it adds the function of DoInteract
+    /// </summary>
 
     private void OnEnable()
     {
-        _playerInput.actions["Interact"].performed += OnInteract;
+        playerInput.actions["Interact"].performed += DoInteract;
     }
 
+    /// <summary>
+    /// When E is let go it takes the function of DoInteract away
+    /// </summary>
     private void OnDisable()
     {
-        _playerInput.actions["Interact"].performed -= OnInteract;
+        playerInput.actions["Interact"].performed -= DoInteract;
     }
 
-    private void OnInteract(InputAction.CallbackContext callbackcontext)
+    /// <summary>
+    /// Do interact function allows the raycast to see if an object has the interactable layer and allows the player to interact
+    /// </summary>
+    /// <param name="callbackcontext"></param>
+    private void DoInteract(InputAction.CallbackContext callbackcontext)
     {
+        if (!Physics.Raycast(_transform.position, _transform.forward, out var hit, 1.5f, interactableLayer)) return;
 
+        if (!hit.transform.TryGetComponent(out InteractableObject interactable)) return;
+
+        interactable.Interact();
+
+        Debug.Log("Interact");
     }    
 }
