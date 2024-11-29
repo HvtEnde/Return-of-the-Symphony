05a8c02604d03b1a7e3b33aa71bac83aec0a3653
HvtEnde <97097945@st.deltion.nl>
2024-11-29 16:58:51 +0100

    Script

---
+++ Return of the Symphony/Assets/Programming/OrbDrop/OrbDrop.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbDrop : MonoBehaviour
+{
+
+    #region Orb Variables
+    [SerializeField] GameObject orb;
+    public bool orbCheck = false;
+    #endregion
+
+
+    void Start()
+    {
+       
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    void DropCheck()
+    {
+        
+    }
+}
